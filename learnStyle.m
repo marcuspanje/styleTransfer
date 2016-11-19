7e244db5a4c1a1fbab37a4d20f05279fb548f280
Chen Zhu <Chen Zhu>
2016-11-19 14:12:06 -0800

    poolLayer

--- learnStyle.m
+++ learnStyle.m
@@ -7,10 +7,10 @@
 setup;
 loadNet = 0;
 if loadNet
-  net = load('vgg-face.mat');
-  net = vl_simplenn_tidy(net);
+    net = load('vgg-face.mat');
+    net = vl_simplenn_tidy(net);
 end
-avgImg = net.meta.normalization.averageImage; 
+avgImg = net.meta.normalization.averageImage;
 
 %images must be 244x244
 im = imread('img/khan.jpg');
@@ -23,7 +23,7 @@ imsz = net.meta.normalization.imageSize;
 im0 = generateWhiteNoiseImage(imsz);
 %generated image
 im0_ = bsxfun(@minus,single(im0),avgImg) ;
-  %apply network on layer
+%apply network on layer
 imR = vl_simplenn(net, im0_);
 
 disp('generating new image');
@@ -34,64 +34,67 @@ gradNext = single(imR(L+1).x - imC(L+1).x);
 step = 0.01;%gradient des step size
 Niterations = 20;
 for iter = 1:Niterations
-  %calculate error by back-propagation
-  gradNext = imR(L+1).x - imC(L+1).x;
-  if mod(iter, 2) == 0
-    err = gradNext.^2;
-    err = sum(sum(sum(err)));
-    disp(sprintf('iteration %03d, err: %d', iter, err));
-  end
-
-  for layer = fliplr(1:L)
-      
-      
-    type = net.layers{layer}.type;
-    szYprev = size(imR(layer).x);
-    grad = zeros(szYprev);
-
-     if strcmp(type, 'conv')
-      weights = net.layers{layer}.weights{1};
-      pad = net.layers{layer}.pad;
-      stride = net.layers{layer}.stride;
-      Yprev = imR(layer).x;
-      
-      [grad,~,~] = vl_nnconv(Yprev, weights, [], gradNext, ...
-        'pad', pad, 'stride', stride);
-
-    elseif strcmp(type, 'softmax')
-      %gradient = softmaxGD(params) * gradient;
-    elseif strcmp(type, 'relu')
+    %calculate error by back-propagation
+    gradNext = imR(L+1).x - imC(L+1).x;
+    if mod(iter, 2) == 0
+        err = gradNext.^2;
+        err = sum(sum(sum(err)));
+        disp(sprintf('iteration %03d, err: %d', iter, err));
+    end
+    
+    for layer = fliplr(1:L)
         
-
         
-        %DZDX = VL_NNRELU(X, DZDY)
+        type = net.layers{layer}.type;
+        szYprev = size(imR(layer).x);
+        grad = zeros(szYprev);
         
-%        DZDY = gradNext;
-        
-%        DZDY = zeros(szYprev(1), szYprev(2), szYprev(3));
-        
-        for i = 1:szYprev(1)
-            for j = 1:szYprev(2)
-                for k = 1:szYprev(3)
-                    if(imR(layer).x(i, j, k) < 0)
-                        grad(i, j, k) = gradNext(i, j, k);
+        if strcmp(type, 'conv')
+            weights = net.layers{layer}.weights{1};
+            pad = net.layers{layer}.pad;
+            stride = net.layers{layer}.stride;
+            Yprev = imR(layer).x;
+            
+            [grad,~,~] = vl_nnconv(Yprev, weights, [], gradNext, ...
+                'pad', pad, 'stride', stride);
+            
+        elseif strcmp(type, 'softmax')
+            %gradient = softmaxGD(params) * gradient;
+        elseif strcmp(type, 'relu')
+            
+            
+            
+            %DZDX = VL_NNRELU(X, DZDY)
+            
+            %        DZDY = gradNext;
+            
+            %        DZDY = zeros(szYprev(1), szYprev(2), szYprev(3));
+            
+            for i = 1:szYprev(1)
+                for j = 1:szYprev(2)
+                    for k = 1:szYprev(3)
+                        if(imR(layer).x(i, j, k) < 0)
+                            grad(i, j, k) = gradNext(i, j, k);
+                        end
                     end
                 end
             end
+            
+            %grad = vl_nnrelu(imR(L+1).x, DZDY);
+            
+        elseif strcmp(type, 'pool')
+            %gradient = reluGD(pool) * graident;
+            Yprev = imR(layer).x;
+            pool = net.layers{layer}.pool;
+            grad = vl_nnpool(Yprev,pool,gradNext);
         end
+        gradNext = single(grad);
         
-        %grad = vl_nnrelu(imR(L+1).x, DZDY);
-      
-    elseif strcmp(type, 'pool')
-      %gradient = reluGD(pool) * graident;
-    end 
-    gradNext = single(grad);
-
-  end %for each layer
-
-  imR(1).x = imR(1).x - step*grad;
-  %reapply network on image
-  imR = vl_simplenn(net, imR(1).x);
+    end %for each layer
+    
+    imR(1).x = imR(1).x - step*grad;
+    %reapply network on image
+    imR = vl_simplenn(net, imR(1).x);
 end % for each iteration
 
 imRDisp= uint8(bsxfun(@plus, imR(1).x, avgImg));
