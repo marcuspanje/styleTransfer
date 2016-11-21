0b1f8b670128169fc53a9c7fd3ee9384fe73845e
Daniel Hsu <daniel.hsu7@gmail.com>
2016-11-20 17:26:52 -0800

    more efficient error calculation

--- LayerStyleError.m
+++ LayerStyleError.m
@@ -1,12 +1,6 @@
 function error = LayerStyleError(G, A, N, M)
 
-    error = 0;
-    
-    for r = 1 : size(G,1)
-        for c = 1 : size(G,2)
-            error = error + (G(r,c) - A(r,c))^2;
-        end
-    end
+    [error, ~] = sumsqr(G - A);
 
     error = error / (4 * N^2 * M^2);
 end
