09fffb0cf62d7274b08c572282fe018b823198f1
Marcus Pan <marcuspanje@gmail.com>
2016-12-02 14:27:33 -0800

    combined lbfgs with errors

--- learnContentStyle.m
+++ learnContentStyle.m
@@ -1,15 +1,14 @@
 %setup is untracked by git, as it is different for all users. 
 %create setup.m with one line:
 %run('[path to matconvnet files]/vl_setupnn');
+
 setup;
 
 %desired layers for style learning
-%desiredLayers = gpuArray([3 8 13 20 27]);
-desiredLayers = gpuArray([3 8 13]);
-desiredLayerWeights = gpuArray([1/3 1/3 1/3]);
-%desiredLayerWeights = gpuArray([1/5 1/5 1/5 1/5 1/5]);
+desiredLayers = gpuArray([3 8 13 20 27]);
+desiredLayerWeights = gpuArray([1/5 1/5 1/5 1/5 1/5]);
 %layer for content learning
-L = 13;
+L = 27;
 
 %load trained network
 if exist('net') ~= 1 
@@ -43,13 +42,12 @@ imNew = vl_simplenn(net, gpuArray(im0_));
 
 disp('generating new image');
 
-Niterations = 5000;
+Niterations = 1;
 %step decreases by annealFactor every so often
 annealFactor = gpuArray(0.7);
 
 zerosGpu = zeros(size(imNew(1).x), 'gpuArray');
 %std gradient descent params
-step = gpuArray(1);      %gradient des step size
 
 %grad descent with momentum params
 gamma = 0.7; 
@@ -59,17 +57,18 @@ v = zerosGpu;
 
 %record error every [plotInterval] timesteps
 prevError = 0;
-plotInterval = 5;
+plotInterval = 1;
 plotIndices = plotInterval:plotInterval:Niterations;
 err = zeros(length(plotIndices), 1);
 errContent = err;
 errStyle = err;
 plotI = 1;
 
-gradWeights = gpuArray([1 1 1000 1000]);
+% style content size variation
+gradWeights = gpuArray([0.001 1 0 0]);
 gradWeights = gradWeights ./ sum(gradWeights);
 
-%ADAM parameters:
+%{ADAM parameters:
 mPrev = zerosGpu;
 vPrev = zerosGpu;
 beta1 = gpuArray(0.9);
@@ -77,6 +76,19 @@ beta2 = gpuArray(0.999);
 beta1Power = gpuArray(1);
 beta2Power = gpuArray(1);
 epsilon = gpuArray(1e-8);
+%}
+
+% l-bfgs parameters
+step = gpuArray(0.1);      %gradient des step size
+[h,w,d] = size(imNew(1).x);
+m =gpuArray(5);
+s = zeros(h*w*d,m,'gpuArray');
+y = zeros(h*w*d,m,'gpuArray');
+alpha = zeros(1,m,'gpuArray');
+beta = zeros(1,m,'gpuArray');
+rou = zeros(1,m,'gpuArray');
+gradPrev = zeros(h*w*d,1,'gpuArray');
+
 
 
 for iter = 1:Niterations
@@ -111,7 +123,6 @@ for iter = 1:Niterations
     shiftDown(1:end-1,:,:) = imNew(1).x(1:end-1,:,:)-imNew(1).x(2:end,:,:);
     gradVariation = shiftRight + shiftDown;
 
-
     grad = gradWeights(1)*gradStyle + gradWeights(2)*gradContent +  ...
       gradWeights(3)*gradSize + gradWeights(4)*gradVariation;
 
@@ -123,7 +134,7 @@ for iter = 1:Niterations
     %imNew(1).x = imNew(1).x - v;
 
     %ADAM updatee
-
+%{
     m = (beta1*mPrev + (1-beta1)*grad);
     v = beta2*vPrev + (1-beta2)*(grad.^2);
     mPrev = m;
@@ -135,6 +146,72 @@ for iter = 1:Niterations
     update = m.*step./(sqrt(v)+epsilon);
     imNew(1).x = imNew(1).x - update;
 
+%}
+
+% ============== l-bfgs update =======================================================   
+   
+    grad1d = mtx2vec(grad);
+    imNew1D = mtx2vec(imNew(1).x);
+    imPrev = imNew1D;
+    
+%     % adjust step size
+%     if(iter > 1 && prev_style_error <= style_error)
+%         step = step/2
+%     end
+        
+    if(iter==1)
+        %standard update
+        imNew(1).x = imNew(1).x - step*grad;  
+        s(:,1) = mtx2vec(imNew(1).x) - imPrev;
+    end %if
+        
+    if(iter >= 2 && iter<=(m+1))
+        q = grad1d
+        y(:,iter-1) = grad1d - gradPrev;
+        rou(iter-1) = 1/(y(:,iter-1)'*s(:,iter-1));
+      
+        for k = (iter-1):-1:1
+            alpha(k) = rou(k)*s(:,k)'*q;
+            q = q - alpha(k)*y(:,k);
+            
+        end %k
+        H0 = (s(:,iter-1)'*y(:,iter-1))/(y(:,iter-1)'*y(:,iter-1));
+        z = H0*q;
+        for k=1:iter-1
+            beta(k) = rou(k)*y(:,k)'*z;
+            z = z + s(:,k)*(alpha(k)-beta(k));
+        end %k
+        
+        imNew(1).x = imNew(1).x - step*vec2mtx(z,h,w,d);
+        s(:,iter) = mtx2vec(imNew(1).x) - imPrev;
+    end %if
+    
+    
+    if(iter > m+1)
+        y(:,1:m-1) = y(:,2:m);
+        y(:,m) = grad1d - gradPrev;
+        rou(1:m-1) = rou(2:m);
+        rou(m) = 1/(y(:,m)'*s(:,m));
+        q = grad1d;
+        
+        for k=m:-1:1
+            alpha(k) = rou(k)*s(:,k)'*q;
+            q = q - alpha(k)*y(:,k);            
+        end %k
+        
+        H0 = s(:,m)'*y(:,m)/(y(:,m)'*y(:,m));
+        z = H0*q;
+        
+        for k=1:m
+            beta(k) = rou(k)*y(:,k)'*z;
+            z = z + s(:,k)*(alpha(k)-beta(k));            
+        end %k
+        gradUpdate = vec2mtx(z,h,w,d);
+        imNew(1).x = imNew(1).x - step*gradUpdate;
+        s(:,1:m-1) = s(:,2:m);
+        s(:,m) = mtx2vec(imNew(1).x) - imPrev;
+    end %if
+% =======================================================================
 
     %reapply image on network
     imNew = vl_simplenn(net, imNew(1).x);
@@ -165,8 +242,10 @@ for iter = 1:Niterations
     end
 end % for each iteration
 
-max(max(imNew(1).x))
-min(min(imNew(1).x))
+img = im2uint8(imNew(1).x);
+img = gather(img);
+save('img1.mat','img');
+
 plotter;
 
 
